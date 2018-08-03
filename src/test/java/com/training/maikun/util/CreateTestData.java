6aad5af4aaa28db5cd1b407956940130850966fd
Cheng <ccelephant2012@gmail.com>
2018-08-03 15:05:46 +0800

    create a util class in order to make test data

---
+++ src/test/java/com/training/maikun/util/CreateTestData.java
@@ -0,0 +1,23 @@
+package com.training.maikun.util;
+
+import com.training.maikun.products.view.ProductView;
+
+/**
+ * @program: order
+ * @Description: To generate the test data
+ * @author: Mr.Cheng
+ * @date: 2018/8/3 下午2:30
+ */
+public class CreateTestData {
+
+    /**
+    * @Description: To get the ProductView Object
+    * @Param: []
+    * @return: com.training.maikun.products.view.ProductView
+    * @Author: Mr.Cheng
+    * @Date: 2018/8/3 下午2:34
+    */
+    static public ProductView getProductView(){
+        return  null;
+    }
+}
