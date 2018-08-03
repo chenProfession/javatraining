3e924a5403418cd1e0a732b0eedb11136d7d925a
Cheng <ccelephant2012@gmail.com>
2018-08-03 16:52:26 +0800

    get a new branch cheng

---
+++ src/main/java/com/training/maikun/result/ResultService.java
@@ -0,0 +1,13 @@
+package com.training.maikun.result;
+
+public interface ResultService {
+
+    /**
+    * @Description: to make REst
+    * @Param: []
+    * @return: com.training.maikun.result.ResultView
+    * @Author: Mr.Cheng
+    * @Date: 2018/8/3 下午3:15
+    */ 
+    ResultView getResultView();
+}
